292b45eaa40210549d64b36a2259d0ba8015533f
William <bobliam20gitlm@gmail.com>
2022-06-25 17:03:21 +0300

    Django CRUD functionality

---
+++ I4G012027PLE/blog/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+
+class PostListView(ListView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
